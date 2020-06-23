610e4ae107c624889e04e713c7514759de89878d
Diego Torres <diegotc86@gmail.com>
2020-06-23 15:47:45 -0500

    Add card references to checklist
    Co-authored-by: Deyvi Conde <iamdeyvi@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_23_203918) do
+ActiveRecord::Schema.define(version: 2020_06_23_204538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,8 @@ ActiveRecord::Schema.define(version: 2020_06_23_203918) do
     t.integer "pos"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "card_id", null: false
+    t.index ["card_id"], name: "index_checklists_on_card_id"
   end
 
   create_table "labels", force: :cascade do |t|
@@ -87,6 +89,7 @@ ActiveRecord::Schema.define(version: 2020_06_23_203918) do
   add_foreign_key "boards", "users"
   add_foreign_key "cards", "lists"
   add_foreign_key "check_items", "checklists"
+  add_foreign_key "checklists", "cards"
   add_foreign_key "labels", "boards"
   add_foreign_key "labels", "cards"
   add_foreign_key "lists", "boards"
